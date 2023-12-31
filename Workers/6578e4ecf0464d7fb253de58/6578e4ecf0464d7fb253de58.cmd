fd177ff35a994ea91cccad963d684618bf88a045
User Teck Solutions <150546088+utsteck@users.noreply.github.com>
2023-12-31 21:01:19 +0600

    v0.0.1

--- Workers/6578e4ecf0464d7fb253de58/6578e4ecf0464d7fb253de58.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute '6578e4ecf0464d7fb253de58.exe' with Python DLL being found.
-set PATH=C:\PYTHON~2;%PATH%
-set PYTHONHOME=C:\Python310
-"%~dp06578e4ecf0464d7fb253de58.exe" %*
